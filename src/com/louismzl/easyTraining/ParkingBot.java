a7ca80084f81c4ae22d680be33579a56239afacf
Louis Mazzella <louis.mazzella@outlook.fr>
2024-04-12 15:04:55 +0200

    p1c4 ok

--- src/com/louismzl/easyTraining/ParkingBot.java
+++ src/com/louismzl/easyTraining/ParkingBot.java
@@ -1,33 +1,41 @@
 package com.louismzl.easyTraining;
 
+import com.louismzl.easyTraining.service.ParkingService;
+
 public class ParkingBot {
+
+    // Variable de type Globale
+    static String completion = "de la part des équipes du Parking Pinguin !";
+    static ParkingService parkingService = new ParkingService();
 
     /**
      * Fonction principale du programme.
-     * @param args Données passées en paramètre lors du lancement du programme.
+     * @param args Tableau de données passées en paramètre lors du lancement du programme.
      */
     public static void main(String[] args) {
-        sayHello(args[0]);
+        sayHello();
+        sayBye();
     }
 
     /**
      * Affiche les messages d'accueil au visiteur.
-     * @param arg Nom du visiteur.
      */
-    private static void sayHello(String arg) {
-        String welcomeMessage = "Bonjour " + arg + ", bienvenue au Parking Pinguin";
-        String services = "Services proposés: Gardienage, laverie ...";
-        String warning = "Dépechez-vous car il n'y aura bientôt plus de place !";
-
+    private static void sayHello() {
+        String welcomeMessage = "Bonjour et bienvenue " + completion;
         System.out.println(welcomeMessage);
-        System.out.println(services);
-        System.out.println(warning.toUpperCase());
+
+        String parkService = parkingService.parking;
+        String washService = parkingService.washing;
+        System.out.println("Services proposés : " + parkService + " & " + washService);
     }
 
-    /*
-    Compilation de la classe (à utiliser après chaque modification):
-    javac src/com/louismzl/easyTraining/ParkingBot.java
-    Exécution de la classe:
-    java --class-path src com.louismzl.easyTraining.ParkingBot <args>
+    /**
+     * Affiche les messages d'au revoir au visiteur.
      */
+    private static void sayBye() {
+        String goodbyeMessage = "Au revoir et à bientôt " + completion;
+        System.out.println(goodbyeMessage);
+    }
+
+
 }
---
+++ src/com/louismzl/easyTraining/service/ParkingService.java
@@ -0,0 +1,8 @@
+package com.louismzl.easyTraining.service;
+
+public class ParkingService {
+
+    public String parking = "Parking et surveillance de véhicules";
+    public String washing = "Nettoyage de véhicules";
+
+}
