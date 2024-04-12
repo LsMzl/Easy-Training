d1e3d7006a89ca2e8095a6d51cf1a12babaa933b
Louis Mazzella <louis.mazzella@outlook.fr>
2024-04-12 17:35:50 +0200

    p1c5 ok

--- src/com/louismzl/easyTraining/ParkingBot.java
+++ src/com/louismzl/easyTraining/ParkingBot.java
@@ -1,39 +1,71 @@
 package com.louismzl.easyTraining;
 
-import com.louismzl.easyTraining.service.ParkingService;
+
+import java.security.SecureRandom;
 
 public class ParkingBot {
 
-    // Variable de type Globale
-    static String completion = "de la part des équipes du Parking Pinguin !";
-    static ParkingService parkingService = new ParkingService();
+    static String[] customers =
+            {"Tom", "Ethan", "Louis", "Camille", "Alex", "Océane", "Gabriel", "Bruce"};
+    
+    // Génération d'un nombre entre 0 et 7
+    static int parkingSize = new SecureRandom().nextInt(10);
+
 
     /**
      * Fonction principale du programme.
      * @param args Tableau de données passées en paramètre lors du lancement du programme.
      */
     public static void main(String[] args) {
-        sayHello();
-        sayBye();
+        // Boucle for classique
+        /*for (int i = 0; i < customers.length; i++) {
+            sayHello(customers[i] );
+        }*/
+
+        // Boucle for améliorée
+        /*for (String customer:customers) {
+            sayHello(customer);
+        }*/
+
+        // Boucle while
+        System.out.println("Il y a " + parkingSize + " places restante(s).");
+        int customerNumber = 0;
+
+        while (parkingSize > 0) {
+            parkingSize--;
+
+            // Si le client = Alex
+            if("Alex".equals(customers[customerNumber])) {
+                customerNumber++;
+                // On ignore Alex et on passe au client suivant (on "saute" une itération)
+                parkingSize++;
+                continue;
+            }
+            // Si le client = Gabriel
+            if("Gabriel".equals(customers[customerNumber])) {
+                System.out.println("Attention, le client Gabriel est ici ! \nFermeture du parking...");
+                // On stoppe la boucle
+                break;
+            }
+            sayHello(customers[customerNumber]);
+            customerNumber++;
+        }
+
     }
 
     /**
      * Affiche les messages d'accueil au visiteur.
      */
-    private static void sayHello() {
-        String welcomeMessage = "Bonjour et bienvenue " + completion;
+    private static void sayHello(String customer) {
+        String welcomeMessage = "Bonjour et bienvenue " + customer;
         System.out.println(welcomeMessage);
-
-        String parkService = parkingService.parking;
-        String washService = parkingService.washing;
-        System.out.println("Services proposés : " + parkService + " & " + washService);
     }
 
     /**
      * Affiche les messages d'au revoir au visiteur.
      */
     private static void sayBye() {
-        String goodbyeMessage = "Au revoir et à bientôt " + completion;
+        String goodbyeMessage = "Au revoir et à bientôt ";
         System.out.println(goodbyeMessage);
     }
 
