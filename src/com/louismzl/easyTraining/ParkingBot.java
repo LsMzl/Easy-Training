d3ca518832e2b2960b959a3b01f4bd8900fc9f25
Louis Mazzella <louis.mazzella@outlook.fr>
2024-04-13 19:33:10 +0200

    ok

--- src/com/louismzl/easyTraining/ParkingBot.java
+++ src/com/louismzl/easyTraining/ParkingBot.java
@@ -18,6 +18,13 @@ public class ParkingBot {
      */
     public static void main(String[] args) {
         processVehicles();
+        //Passage par valeur
+       /* int a = 10;
+        int b = a;
+        System.out.println("a = " + a + "\nb = " + b);
+
+        a = 15;
+        System.out.println("a = " + a + "\nb = " + b);*/
     }
 
     /**
@@ -26,72 +33,14 @@ public class ParkingBot {
      * et la date d'entrée.
      */
     private static void processVehicles() {
-        // Déclaration d'une liste de tickets (ensemble d'éléments ordonnés)
-        //List<Tickets> tickets = new ArrayList<>();
-
-        //Déclaration d'un ensemble d'éléments non ordonnés mais unique
-        Set<Tickets> tickets = new HashSet<>();
 
         Vehicles vehicle = new Vehicles();
         vehicle.setRegistrationNumber("AZ-244-LV");
         Tickets vehicleTicket = parkingService.processIncomingVehicle(vehicle);
-        tickets.add(vehicleTicket);
+        System.out.println(vehicleTicket);
+        parkingService.processExitingVehicle(vehicleTicket);
+        System.out.println(vehicleTicket);
+    }
 
-        Vehicles yamaha = new Bicycles();
-        yamaha.setRegistrationNumber("MT-456-YH");
-        Tickets bicycleTicket = parkingService.processIncomingVehicle(yamaha);
-        tickets.add(bicycleTicket);
-
-        Vehicles peugeot = new Cars();
-        peugeot.setRegistrationNumber("FF-014-JN");
-        Tickets carTicket = parkingService.processIncomingVehicle(peugeot);
-        tickets.add(carTicket);
-
-        // Déclaration d'un tableau contenant 3 tickets
-        /*Tickets[] tickets = new Tickets[3];
-
-        tickets[0] = vehicleTicket;
-        tickets[1] = bicycleTicket;
-        tickets[2] = carTicket;*/
-
-
-        System.out.println("Traitement des sorties par lot");
-
-        // Récupération de l'index des tickets et de leur données
-        Map<Integer, Tickets> exitOrder = new HashMap<>();
-        int position = 0;
-
-        // Parcours des tickets
-        for (Tickets ticket: tickets) {
-            // On ajoute dans la map la position(index) et le ticket
-            exitOrder.put(position, ticket);
-            // Incrémentation de la position
-            position++;
-            // Affichage des tickets
-            System.out.println(parkingService.processExitingVehicle(ticket));
-        }
-
-            // Affichage des positions
-            Set<Integer> keySet = exitOrder.keySet();
-
-            for (int key: keySet) {
-                System.out.println("Position dans la liste : " + key + " \n " + exitOrder.get(key).getVehicule().getCategory());
-
-            }
-        }
-
-        // Parcours du tableau
-        /*for (int i = 0; i < tickets.length; i++) {
-            System.out.println(parkingService.processExitingVehicle(tickets[i]));
-        }*/
-
-        //Parcours d'une liste
-        /*for (int i = 0; i < tickets.size(); i++) {
-            System.out.println(parkingService.processExitingVehicle(tickets.get(i)));
-        }*/
-        // ou
-        /*for (Tickets ticket : tickets) {
-            System.out.println(parkingService.processExitingVehicle(ticket));
-        }*/
 
 }
